11e22794d6fc28ce143a8b1ffe9172bb7ce422bc
WaheedShaq <waheedshaqra@gmail.com>
2022-07-02 15:11:24 +0300

    Testing out DOM events and creating a To-Do-List using JS

--- javascript.js
+++ javascript.js
@@ -1,50 +1,32 @@
-var database = [
-  {
-    username: "andrei",
-    password: "supersecret",
-  },
-  {
-    username: "Sally",
-    password: "123",
-  },
-  {
-    username: "Ingrid",
-    password: "777",
-  },
-];
+var button = document.getElementById("enter");
+var input = document.getElementById("user-input");
+var ul = document.querySelector("ul");
 
-var newsFeed = [
-  {
-    username: "Bobby",
-    timeline: "So tired from all that learning",
-  },
-  {
-    username: "Sally",
-    timeline: "JS is cool!",
-  },
-  {
-    username: "Mitch",
-    timeline: "JS is nice!",
-  },
-];
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
-
-function isUserValid(user, pass) {
-  for (var i = 0; i < database.length; i++) {
-    if (database[i].username === user && database[i].password === pass) {
-      return true;
-    }
-  }
-  return false;
+function inputLength() {
+  return input.value.length;
 }
 
-function signIn(user, pass) {
-  if (isUserValid(user, pass)) {
-    console.log(newsFeed);
-  } else {
-    alert("Wrong Username and Password");
+function createListElement() {
+  if (inputLength() > 0) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
   }
 }
 
-signIn(userNamePrompt, passwordPrompt);
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeyPress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterClick());
+
+input.addEventListener("keypress", addListAfterKeyPress);
